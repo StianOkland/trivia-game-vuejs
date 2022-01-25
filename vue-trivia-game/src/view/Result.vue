99ab329a0595f9d664e4eef548720f12d4505879
Isak Haknes <isakhauknes@gmail.com>
2022-01-25 15:26:16 +0100

    FEAT: Table for quiz results and button + function to return to HomeScreen

--- vue-trivia-game/src/view/Result.vue
+++ vue-trivia-game/src/view/Result.vue
@@ -1,7 +1,43 @@
 <script setup>
+import { computed, onMounted, ref } from 'vue';
+import { useStore } from 'vuex';
+import { useRouter } from 'vue-router';
+
+const store = useStore();
+const router = useRouter();
+
+const userAnswers = computed(() => store.getters.getUserAnswers) 
+
+const newQuiz = () => {
+    router.push({
+        name: 'HomeScreen',
+    });
+};
+
+// TODO: function to check if score is new highscore
 </script>
 
 <template>
+    <h1>Quiz result</h1>
+    <h2>Highscore: 500</h2> <!-- TODO: get users highscore -->
+
+    <table>
+        <thead>
+            <tr>
+                <th>Question</th>
+                <th>Your answer</th>
+                <th>Correct answer</th>
+            </tr>
+        </thead>
+        <tbody> <!-- TODO: loop through both userAnswers and questions -->
+            <tr v-for="answer in userAnswers">
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+
+    <button @click="restart"> Restart Quiz </button>
+
 </template>
 
 <style scoped>
