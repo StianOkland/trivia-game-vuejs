73c5006a798b63942df4b54e7b62d4aa0d85a73c
Isak Hauknes <isakhauknes@gmail.com>
2022-01-26 01:04:08 +0100

    FEAT: Display score, Display questions with correct, users answer, Homebutton and untested score to API if new highscore

--- vue-trivia-game/src/view/Result.vue
+++ vue-trivia-game/src/view/Result.vue
@@ -1,40 +1,84 @@
 <script setup>
-import { computed, onMounted, ref } from 'vue';
+import { computed, onMounted} from 'vue';
 import { useStore } from 'vuex';
 import { useRouter } from 'vue-router';
+
+onMounted(async () => {
+    await store.dispatch('fetchUsers')
+})
 
 const store = useStore();
 const router = useRouter();
 
 const userAnswers = computed(() => store.getters.getUserAnswers) 
+const users = computed(() => store.state.users)
+const questions = computed(() => store.getters.getQuestions)
+let score = 0;
 
-const newQuiz = () => {
+const updateHighScore = async () => {
+    await store.dispatch('updateHighScore', score)
+}
+
+const getScore = () => {
+    for(let i = 0; i < questions.value.length; i++) {
+        if(userAnswers.value[i] === questions.value[i].correct_answer) {
+           score += 10;
+        }
+    }
+    updateHighScore();
+}
+
+const restart = () => {
     router.push({
         name: 'HomeScreen',
     });
 };
 
-// TODO: function to check if score is new highscore
+
+getScore();
 </script>
 
 <template>
-    <h1>Quiz result</h1>
-    <h2>Highscore: 500</h2> <!-- TODO: get users highscore -->
+    <h1> Quiz result </h1>
+    <h2> Highscore: {{ score }} </h2>
 
-    <table>
+    <!-- <table>
         <thead>
             <tr>
-                <th>Question</th>
-                <th>Your answer</th>
-                <th>Correct answer</th>
+                <th> Question </th>
+                <th> Your answer </th>
+                <th> Correct answer </th>
             </tr>
         </thead>
-        <tbody> <!-- TODO: loop through both userAnswers and questions -->
-            <tr v-for="answer in userAnswers">
-                <td></td>
+        <tbody>
+            <tr>
+                <div v-for="question in questions" :key="question.question">
+                    <td>{{ question.question }}</td>
+                </div>
+                <div v-for="userAnswer in userAnswers" :key="userAnswer.value">
+                    <td>{{ userAnswer }}</td>
+                </div>
+                <div v-for="question in questions" :key="question.correct_answer">
+                    <td>{{ question.correct_answer }}</td>
+                </div>
             </tr>
         </tbody>
-    </table>
+    </table> -->
+
+    <div>
+        <h2> Questions </h2>
+        <div v-for="question in questions" :key="question.question">
+            <label>{{ question.question }}</label>
+        </div>
+        <h2> Your answers </h2>
+        <div v-for="userAnswer in userAnswers" :key="userAnswer.value">
+            <label>{{ userAnswer }}</label>
+        </div>
+        <h2> Correct answers </h2>
+        <div v-for="question in questions" :key="question.correct_answer">
+            <label>{{ question.correct_answer }}</label>
+        </div>
+    </div>
 
     <button @click="restart"> Restart Quiz </button>
 
